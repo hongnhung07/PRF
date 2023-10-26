e9cba4935ab84dd69edca835ffd211c4b77723fe
HongNhung07 <nhunghoang829@gmail.com>
2023-10-26 08:14:09 +0700

    string

--- tong3chancuoi.c
+++ tong3chancuoi.c
@@ -3,21 +3,21 @@
 #include <string.h> //lam viec voi cac ham ve chuoi ky tu strlen
 #include <math.h>	//lam viec voi cac ham toan sin, cos, abs...pow,
 #include <ctype.h>	//lam viec voi cac ham ky tu
-
-
 int main() {
-
-   	int n,sum=0,i,cuoi;
-   	scanf("%d", &n);
-   	if (n%2==0) {
-	   cuoi=n;
-   	sum= cuoi + (cuoi-2) + (cuoi -4);
-   	}
-   	else { cuoi=n-1;
-   	sum= cuoi + (cuoi-2) + (cuoi -4);
-   	}
+   	int n,sum=0,i,dem=0;
+   	scanf("%d",&n);
+   	for (i=n; i>0;i--){
+   		if(i%2==0){
+   			sum+=i;
+   			dem++;
+		   }
+		   if(dem==3){
+		   	break;
+		   }
+	   }
+   	printf("\nOUTPUT:\n");
    	printf("%d", sum);
-	   	
- 
+   	printf("\n");
+  system ("pause"); 
   return(0);		// quay ve
 }
